3bd1c8e0e74e5e37d9b0975b9ae59d700931b2d7
Max <mletellier02@gmail.com>
2024-02-21 11:18:48 +0000

    error handeling

--- src/main/java/project/notification.java
+++ src/main/java/project/notification.java
@@ -26,7 +26,7 @@ public class Notification {
     private static final String APPLICATION_NAME = "Project Notfication";
     private static final List<String> SCOPES = Collections.singletonList("https://www.googleapis.com/auth/gmail.readonly"); // Read only
     private static final String CREDENTIALS_FILE_PATH = "token\\client_secret_451378032531-5i5ui21h5hm3dij14h7m0oikhqr9mksu.apps.googleusercontent.com.json"; // Creds of API
-    private final Gmail service;
+    private static Gmail service;
 
     public Notification() throws Exception{
         NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
@@ -38,23 +38,26 @@ public class Notification {
 
     private static Credential getCredentials(final NetHttpTransport httpTransport, GsonFactory JSON_FACTORY) throws IOException {
         // Load client secrets.
-        InputStream in = Notification.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (in == null) {
-            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        try (InputStream in = Notification.class.getResourceAsStream(CREDENTIALS_FILE_PATH)) {
+            if (in == null) {
+                throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+            }
+            GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+            // Build flow and trigger user authorization request.
+            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+                .setAccessType("offline")
+                .build();
+            LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+            Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+            return credential;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
         }
-        GoogleClientSecrets clientSecrets =
-            GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-            httpTransport, JSON_FACTORY, clientSecrets, SCOPES)
-            .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
-            .setAccessType("offline")
-            .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-
-        return credential;
     }
 
     // Gets lists of the unreadmessages
@@ -75,20 +78,24 @@ public class Notification {
         return "No subject";
     }
 
-    public static void getNotification(String userEmail, ArrayList<String> searchEmail) throws Exception{
+    public static void getNotification(ArrayList<String> searchEmail) throws Exception{
 
         Notification notification = new Notification();
 
-        List<List<Message>> unreadMessages = new ArrayList<>();
-        for(String emails : searchEmail){
-            unreadMessages.add(notification.getUnreadMessages(emails));
-        }
-
-        // Process Message
-        for (List<Message> userM : unreadMessages){
-            for (Message message : userM) {
-                System.out.println(message + " :Subject: " + notification.getMessageSubject(message.getId()) + "\n");
+        try{
+            List<List<Message>> unreadMessages = new ArrayList<>();
+            for(String emails : searchEmail){
+                unreadMessages.add(notification.getUnreadMessages(emails));
             }
+
+            // Process Message
+            for (List<Message> userM : unreadMessages){
+                for (Message message : userM) {
+                    System.out.println(message + " :Subject: " + notification.getMessageSubject(message.getId()) + "\n");
+                }
+            }
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
         }
 
     }
